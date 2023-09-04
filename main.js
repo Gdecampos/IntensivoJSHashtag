85516cf1d91bd9a100305189cfcd38d41349e3d6
gdecampos <gdecamposjst@gmail.com>
2023-09-04 03:16:22 -0300

    commit do projeto do intensivo javascript da hashtag

---
+++ main.js
@@ -0,0 +1,12 @@
+import { renderizarCatalogo } from "./src/cartaoProduto";
+import { inicializarFiltros } from "./src/filtrosCatalogo";
+import { 
+    inicializarCarrinho, 
+    atualizarPrecoCarrinho, 
+    renderizarProdutosCarrinho } from "./src/menuCarrinho";
+
+renderizarCatalogo();
+inicializarCarrinho();
+renderizarProdutosCarrinho();
+atualizarPrecoCarrinho();
+inicializarFiltros();
